238965984c37a16882696a52ed16c4d7fa19a746
Nathan Pillai <natepill@gmail.com>
2019-03-02 14:26:19 -0800

    GIT removed cached for env and git ignore

--- FinalProject/env.py
+++
@@ -1,4 +0,0 @@
-'''For some reason, .bash-profile env variables are not working, so the hacky solution is to add this file to my gitignore and import file into the project'''
-
-TWITTER_APP_KEY='Pkk8FlrqP1K2rAXUsTQuirpoo'
-TWITTER_APP_SECRET='DjzupYW7EF6IgcXaAapyGksXuMjD2yktnHlnxCMLXxduwKEz9y'
